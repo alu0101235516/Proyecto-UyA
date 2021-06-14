42a99b003104652248318aefacd2b6edd214b33f
Borja Guanche <72826199+alu0101205908@users.noreply.github.com>
2021-06-14 23:00:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -200,15 +200,3 @@ function removeDatos(elemento){
     location.reload();
 
 }
-
-
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
-  });
